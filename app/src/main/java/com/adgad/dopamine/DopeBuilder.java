68116cbac87c486c0294e9b3424c82d8aa56d8c0
Arjun Gadhia <arjungadhia@gmail.com>
2020-09-24 12:23:53 +0100

    Show images from gallery, different posts types, icons

--- app/src/main/java/com/adgad/dopamine/DopeBuilder.java
+++ app/src/main/java/com/adgad/dopamine/DopeBuilder.java
@@ -1,5 +1,6 @@
 package com.adgad.dopamine;
 
+import android.Manifest;
 import android.app.AlarmManager;
 import android.app.Notification;
 import android.app.PendingIntent;
@@ -9,18 +10,24 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Environment;
 import android.os.SystemClock;
 import android.provider.ContactsContract;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 
 import androidx.preference.PreferenceManager;
 
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Random;
+
+import static java.util.Map.entry;
 
 public class DopeBuilder {
 
@@ -30,11 +37,32 @@ public class DopeBuilder {
     private Random generator = new Random();
 
     private static int ONE_DAY = 86400000;
+    private static final String CAMERA_IMAGE_BUCKET_NAME =
+            Environment.getExternalStorageDirectory().toString()
+                    + "/DCIM/Camera";
+    public static final String CAMERA_IMAGE_BUCKET_ID =
+            getBucketId(CAMERA_IMAGE_BUCKET_NAME);
+
+    private static final String[] NOTIFICATION_TYPES = new String[]{
+        "tweet_like", "post_like", "image_like", "tweet_reply", "post_reply", "image_reply"
+    };
+
+    String[] instaReplies;
+    String[] tweetReplies;
 
 //save the context recievied via constructor in a local variable
-
+    /**
+     * Matches code in MediaProvider.computeBucketValues. Should be a common
+     * function.
+     */
+    public static String getBucketId(String path) {
+        return String.valueOf(path.toLowerCase().hashCode());
+    }
     public DopeBuilder(Context context){
         this.context=context;
+        instaReplies = context.getResources().getStringArray(R.array.image_replies);
+        tweetReplies = context.getResources().getStringArray(R.array.text_replies);
+
     }
 
     public void sendDopamineHit() {
@@ -47,6 +75,42 @@ public class DopeBuilder {
 
         this.scheduleNotification(this.getNotification(contact[0], contact[1]), delay);
     }
+
+
+    private Bitmap getRecentImage() {
+            final String[] projection = { MediaStore.Images.Media.DATA };
+            final String selection = MediaStore.Images.Media.BUCKET_ID + " = ?";
+            final String[] selectionArgs = { CAMERA_IMAGE_BUCKET_ID };
+            final Cursor cursor = context.getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                    projection,
+                    selection,
+                    selectionArgs,
+                    null);
+            ArrayList<String> result = new ArrayList<String>(cursor.getCount());
+            if (cursor.moveToFirst()) {
+                final int dataColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                do {
+                    final String data = cursor.getString(dataColumn);
+                    result.add(data);
+                } while (cursor.moveToNext());
+            }
+            cursor.close();
+
+            if(result.size() > 0) {
+                String fileName = result.get(this.generator.nextInt(result.size()));
+                File imgFile = new File(fileName);
+                if(imgFile.exists()){
+                    BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+                    bmOptions.inSampleSize = 2;
+                    bmOptions.inJustDecodeBounds = false;
+                    Bitmap bitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath(),bmOptions);
+                    return bitmap;
+                }
+            }
+            return null;
+        }
+
+
 
     private String[] getRandomContact() {
         Cursor managedCursor = context.getContentResolver().query(ContactsContract.Contacts.CONTENT_URI,null, null, null, null);
@@ -75,32 +139,82 @@ public class DopeBuilder {
 
     }
 
-    private String getRandomTitle() {
-        String[] apps = new String[]{"Instagram", "Twitter", "Facebook"};
-        return apps[this.generator.nextInt(apps.length)];
-    }
 
-    private String getRandomText() {
-        String[] text = new String[]{"liked your tweet", "replied to your tweet", "liked your photo", "liked your story"};
-        return text[this.generator.nextInt(text.length)];
-    }
+    private Notification getNotification(String name, String userImage) {
 
-    private Notification getNotification(String name, String image) {
+        String type = NOTIFICATION_TYPES[this.generator.nextInt(NOTIFICATION_TYPES.length)];
         Notification.Builder builder = new Notification.Builder(this.context);
-        builder.setContentTitle(getRandomTitle());
-        builder.setContentText(name + " " + getRandomText());
-        InputStream inputStream = null;
-        try {
-            if(image != null) {
-                Uri imageUri = Uri.parse(image);
-                Bitmap icon = MediaStore.Images.Media.getBitmap(this.context.getContentResolver(), imageUri);
-                builder.setLargeIcon(icon);
-            }
-            builder.setSmallIcon(R.drawable.ic_launcher_foreground);
-
-        } catch (IOException e) {
-            builder.setSmallIcon(R.drawable.ic_launcher_foreground);
+        String text;
+        String actor = name;
+        int others = this.generator.nextInt(13) - 7;
+        if(others > 0) {
+            actor += " and " + others + " others";
         }
+        switch(type) {
+            case "tweet_like":
+                builder.setContentTitle("Twitter");
+                builder.setSmallIcon(R.drawable.ic_favorite);
+
+                builder.setContentText(actor + " liked your tweet");
+                break;
+            case "post_like":
+                builder.setContentTitle("Facebook");
+                builder.setSmallIcon(R.drawable.ic_favorite);
+
+                builder.setContentText(actor + " liked your post");
+                break;
+            case "image_like":
+                builder.setContentTitle("Instagram");
+                builder.setSmallIcon(R.drawable.ic_favorite);
+
+                builder.setContentText(actor + " liked your photo");
+                break;
+            case "tweet_reply":
+                builder.setContentTitle("Twitter");
+                builder.setSmallIcon(R.drawable.comment);
+
+                text = actor + " replied to your tweet: "  + tweetReplies[this.generator.nextInt(tweetReplies.length)];
+                builder.setStyle(new Notification.BigTextStyle()
+                        .bigText(text));
+                builder.setContentText(text);
+                break;
+            case "post_reply":
+                builder.setContentTitle("Facebook");
+                builder.setSmallIcon(R.drawable.comment);
+
+                text = actor + " commented on your post: "  + tweetReplies[this.generator.nextInt(tweetReplies.length)];
+                builder.setStyle(new Notification.BigTextStyle()
+                        .bigText(text));
+                builder.setContentText(text);
+                break;
+            case "image_reply":
+                builder.setContentTitle("Instagram");
+                builder.setSmallIcon(R.drawable.comment);
+                builder.setContentText(name + " commented on your photo: " + instaReplies[this.generator.nextInt(instaReplies.length)]);
+                break;
+        }
+        builder.setSmallIcon(R.drawable.ic_favorite);
+
+        if(userImage != null) {
+            Uri imageUri = Uri.parse(userImage);
+            Bitmap icon = null;
+            try {
+                icon = MediaStore.Images.Media.getBitmap(this.context.getContentResolver(), imageUri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            builder.setLargeIcon(icon);
+        }
+
+        if(type.contains("image")) {
+            Bitmap recentImage = getRecentImage();
+
+            builder.setStyle(new Notification.BigPictureStyle()
+                    .bigPicture(recentImage));
+
+
+        }
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             builder.setChannelId("dopamine_hit");
         }
--- app/src/main/java/com/adgad/dopamine/SettingsActivity.java
+++ app/src/main/java/com/adgad/dopamine/SettingsActivity.java
@@ -1,12 +1,26 @@
 package com.adgad.dopamine;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.preference.PreferenceFragmentCompat;
 
-public class SettingsActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class SettingsActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
+
+    String[] permissions = new String[]{
+            Manifest.permission.READ_CONTACTS,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,18 +30,47 @@ public class SettingsActivity extends AppCompatActivity {
                 .beginTransaction()
                 .replace(R.id.settings, new SettingsFragment())
                 .commit();
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
+        if(checkPermissions()) {
+            DopeBuilder dopeBuilder = new DopeBuilder(this);
+            dopeBuilder.sendDopamineHit();
         }
-        DopeBuilder dopeBuilder = new DopeBuilder(this);
-        dopeBuilder.sendDopamineHit();
+    }
+
+    private boolean checkPermissions() {
+        int result;
+        List<String> listPermissionsNeeded = new ArrayList<>();
+        for (String p : permissions) {
+            result = ContextCompat.checkSelfPermission(this, p);
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                listPermissionsNeeded.add(p);
+            }
+        }
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(this, listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]), 100);
+            return false;
+        }
+        return true;
     }
 
     public static class SettingsFragment extends PreferenceFragmentCompat {
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        if (requestCode == 100) {
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if(checkPermissions()) {
+                    DopeBuilder dopeBuilder = new DopeBuilder(this);
+                    dopeBuilder.sendDopamineHit();
+                }
+            }
+            return;
         }
     }
 }
